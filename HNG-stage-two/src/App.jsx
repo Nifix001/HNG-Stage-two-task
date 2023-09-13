51c9168ec584aa6b03558de991c72d82c542db2d
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-09-13 18:39:04 +0100

    update

--- HNG-stage-two/src/App.jsx
+++ HNG-stage-two/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom"
+
 
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route>
+
+        </Route>
+      </Route>
+    )
+  )
+
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
