37a46af39d9d1fe0c000320f1461e3c95ad7aa26
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-09-14 19:23:13 +0100

    Update

--- HNG-stage-two/src/App.jsx
+++ HNG-stage-two/src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react"
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom"
+
+import Homepage from "./Pages/Homepage"
+import Movie from "./Pages/Movie"
 
 
 
 function App() {
 
+  const [film, setFilm] = useState([])
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=59c757f8cfc2f4a0b033a50e188ae82c&page=1&primary_release_date.gte=2023&sort_by=popularity.desc&vote_average.gte=7.88&year=2023');
+      if(response.ok){
+        const data = await response.json();
+        const movies = data.results;
+        setFilm(movies);
+      }
+    }
+    fetchMovies();
+  }, [])
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route>
-
-        </Route>
+        <Route index element={<Homepage/>} />
+        <Route path="/i/movies/:id" element={<Movie />}/>
       </Route>
     )
   )
